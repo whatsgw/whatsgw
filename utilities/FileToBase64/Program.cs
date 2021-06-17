b5712b8057229fc94e1303be120735345173a567
Pedro Canadas <phenriquec@gmail.com>
2021-06-17 10:27:36 -0300

    add utilities/FileToBase64 and update webhook.php

---
+++ utilities/FileToBase64/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+
+namespace FileToBase64
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            try
+            {
+                if (args.Length < 2)
+                {
+                    Console.WriteLine("Usage: FileToBase64.exe [sourcefle] [destionationfile]" +
+                        "\n\n" +
+                        "Example: FileToBase64.exe c:\\image.png c:\\imagebase64.txt");
+                    return;
+                }
+
+                string source = args[0];
+                string dest = args[1];
+                string ret = FileToBase64(source);
+
+                if (dest.Equals("s"))
+                    Console.Write(ret);
+                else
+                    File.WriteAllText(dest, ret);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("Fail: " + ex.Message);
+            }
+
+        }
+
+        public static string FileToBase64(string path)
+        {
+            Byte[] bytes = File.ReadAllBytes(path);
+            String file = Convert.ToBase64String(bytes);
+            return file;
+
+        }
+    }
+
+}
+
