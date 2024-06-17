65369cc4f1d32f12cc44fb2d7504c2f689b15a61
Pedro Canadas <phenriquec@gmail.com>
2024-06-17 16:01:42 -0300

    adição de logs

--- PHP/webhook.php
+++ PHP/webhook.php
@@ -1,12 +1,21 @@
 <?php
 
-if($json = json_decode(file_get_contents("php://input"), true)) {
-  print_r($json);
-  $data = $json;
+// Exibir os parâmetros enviados via GET
+if (!empty($_GET)) {
+    echo "GET Parameters:\n";
+    print_r($_GET);
+}
+
+// Verificar e processar o corpo JSON da requisição
+if ($json = json_decode(file_get_contents("php://input"), true)) {
+    echo "JSON Body:\n";
+    print_r($json);
+    $data = $json;
 } else {
-  
-  print_r($_POST);
-  $data = $_POST;
+    // Caso não seja um JSON, exibir os parâmetros POST
+    echo "POST Parameters:\n";
+    print_r($_POST);
+    $data = $_POST;
 }
 
 ?>
